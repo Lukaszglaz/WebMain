bc0875c1a99339aa17e225867ba5247f7b5ce80e
Lukaszglaz <glazzlukasz@gmail.com>
2023-08-20 17:54:30 +0200

    Scroll Animation Test

--- js/animate-scroll.js
+++ js/animate-scroll.js
@@ -1,19 +1,64 @@
-// const boxes = document.querySelectorAll('.box');
+// Scroll Element List
+const clickMainScroll = document.getElementById('section-home');
+const clickProjectScroll = document.getElementById('section-project');
+const clickPortfolioScroll = document.getElementById('section-portfolio');
+const clickBlogScroll = document.getElementById('section-blog');
+const clickContactScroll = document.getElementById('section-contact');
 
-// window.addEventListener('scroll', checkBoxes);
+const scrollToProject = document.getElementById('projectScroll');
+const scrollToPortfolio = document.getElementById('');
+const scrollToBlog = document.getElementById('');
+const scrollToContact = document.getElementById('');
 
-// checkBoxes();
+clickMainScroll.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
-// function checkBoxes() {
-//   const triggerBottom = (window.innerHeight / 5) * 4;
+clickProjectScroll.addEventListener('click', () => {
+  scrollToProject.scrollIntoView({ behavior: 'smooth' });
+});
+// End Scroll Element lIST
 
-//   boxes.forEach((box) => {
-//     const boxTop = box.getBoundingClientRect().top;
+// Active Element List
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
+};
 
-//     if (boxTop < triggerBottom) {
-//       box.classList.add('show');
-//     } else {
-//       box.classList.remove('show');
-//     }
-//   });
-// }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const elementId = entry.target.getAttribute('id'); // Pobieranie ID sekcji
+
+    if (entry.isIntersecting) {
+      handleVisibleElement(elementId);
+    } else {
+      handleInvisibleElement(elementId);
+    }
+  });
+}, options);
+
+const elementIds = [
+  'section-home',
+  'section-project',
+  'section-portfolio',
+  'section-blog',
+  'section-contact',
+];
+
+elementIds.forEach((elementId) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    observer.observe(element);
+  }
+});
+
+function handleVisibleElement(elementId) {
+  elementId.classList.add('active');
+}
+
+function handleInvisibleElement(elementId) {
+  elementId.classList.remove('active');
+}
+
+// End Active Element List
